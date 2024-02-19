d13b9c8f7d0a6ef37b4588b974acc14196ad0fd5
Mike Horn <v-mikehorn@microsoft.com>
2024-02-19 10:38:29 -0500

    Added Contact, Projects, and Services modules with routing support. - Created moduleHome components within each new module. - Configured routing for each new module to include a path to the moduleHome component. - Verified no new application setup and new modules integration compiler errors.

---
+++ src/app/contact/contact-home/contact-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-home',
+  templateUrl: './contact-home.component.html',
+  styleUrl: './contact-home.component.css'
+})
+export class ContactHomeComponent {
+
+}
